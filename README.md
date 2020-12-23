03994c80cad35c7c1bc5ce442f5c9fbfb520e569
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2020-12-23 14:30:40 +0000

    Updated files with JS initial structure. Updated README with thought porcess and roadmap

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ Quiz Website using HTML CSS & JS
 Update History
 
 0.1 Created initial HTML frame & CSS styling for navbar, question, 4 answers and submit button.
+0.2 Added basis JS. Include updating question box with question and answers with relevant answer
+
+
+
+App.JS
+lines 1-5 consts to create event listeners
+lines 7-13 Questions object : To contain the questions and range of answers
+lines 16-20 Updating textConvent with relevant question and answers
+line 23-26 for loop to create event listeners on buttons
+
+
+Roadmap
+Only select one answer when clicked (remove selected class from another answer)
+Create questionbank
+Have questions populate randomly
+Submit button appear
+On submit confirm correct answer
+Maintain score
+Convert to classes / OOP
---
+++ app.js
@@ -0,0 +1,26 @@
+const QuestionBox = document.querySelector('.question-box')
+const A1 = document.getElementById('ans1')
+const A2 = document.getElementById('ans2')
+const A3 = document.getElementById('ans3')
+const A4 = document.getElementById('ans4')
+
+const Questions = {
+  Question: `What is Harry's last name?`,
+  Answer: {Correct:`Potter`,
+  W1: 'Kewell',
+  W2: 'Redknapp',
+  W3: 'Houdini'}
+  }
+  
+
+QuestionBox.textContent = Questions.Question
+A1.textContent = Questions.Answer.Correct
+A2.textContent = Questions.Answer.W1
+A3.textContent = Questions.Answer.W2
+A4.textContent = Questions.Answer.W3
+
+  
+Answer = document.querySelectorAll('.answer')
+  for (a of Answer){
+  a.addEventListener('click',function(){
+  this.classList.toggle('selected')})}
